8c80d1b68dce99734383a5818529dd82af135bbb
antonio.pascucci <antonio.pascucci@performer.it>
2025-12-31 19:52:41 +0100

    Aggiunge CRUD AnnualBudget e link in sidebar

---
+++ src/Controller/AnnualBudgetController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AnnualBudget;
+use App\Form\AnnualBudgetType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AnnualBudgetController extends BaseController
+{
+    public const CONTROLLER_NAME = 'AnnualBudgetController';
+
+    #[Route('/annual-budget/index', name: 'app_annual_budget_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $budgets = $user ? $em->getRepository(AnnualBudget::class)->findAllForUser($user) : [];
+
+        return $this->render('annual_budget/index.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+            'budgets' => $budgets,
+        ]);
+    }
+
+    #[Route('/annual-budget/new', name: 'app_annual_budget_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $budget = new AnnualBudget();
+        $form = $this->createForm(AnnualBudgetType::class, $budget, ['user' => $user]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($budget);
+            $em->flush();
+
+            return $this->redirectToRoute('app_annual_budget_index');
+        }
+
+        return $this->renderTurbo('annual_budget/edit.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+            'budget' => $budget,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/annual-budget/edit/{id}', name: 'app_annual_budget_edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $budget = $em->getRepository(AnnualBudget::class)->findOneForUser($id, $user);
+        if (!$budget) {
+            throw new NotFoundHttpException();
+        }
+
+        $form = $this->createForm(AnnualBudgetType::class, $budget, ['user' => $user]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_annual_budget_index');
+        }
+
+        return $this->renderTurbo('annual_budget/edit.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+            'budget' => $budget,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/annual-budget/delete/{id}', name: 'app_annual_budget_delete', methods: ['GET', 'POST'])]
+    public function delete(int $id, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof \App\Entity\User) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $budget = $em->getRepository(AnnualBudget::class)->findOneForUser($id, $user);
+        if (!$budget) {
+            throw new NotFoundHttpException();
+        }
+
+        $em->remove($budget);
+        $em->flush();
+
+        return $this->redirectToRoute('app_annual_budget_index');
+    }
+}
---
+++ src/Form/AnnualBudgetType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AnnualBudget;
+use App\Entity\Ship;
+use App\Repository\ShipRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnualBudgetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $user = $options['user'];
+
+        $builder
+            ->add('startDay', NumberType::class, [
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('startYear', NumberType::class, [
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('endDay', NumberType::class, [
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('endYear', NumberType::class, [
+                'attr' => ['class' => 'input m-1 w-full'],
+            ])
+            ->add('ship', EntityType::class, [
+                'class' => Ship::class,
+                'placeholder' => '-- Select a Ship --',
+                'choice_label' => fn (Ship $ship) => sprintf('%s (%s)', $ship->getName(), $ship->getClass()),
+                'query_builder' => function (ShipRepository $repo) use ($user) {
+                    $qb = $repo->createQueryBuilder('s')->orderBy('s.name', 'ASC');
+                    if ($user) {
+                        $qb->andWhere('s.user = :user')->setParameter('user', $user);
+                    }
+                    return $qb;
+                },
+                'attr' => ['class' => 'select m-1 w-full'],
+            ])
+            ->add('note', TextareaType::class, [
+                'required' => false,
+                'attr' => ['class' => 'textarea m-1 w-full', 'rows' => 3],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AnnualBudget::class,
+            'user' => null,
+        ]);
+    }
+}
