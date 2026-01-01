491e787e380ab8e31a541be6d2d2e45bf5bc34b0
antonio.pascucci <antonio.pascucci@performer.it>
2026-01-01 14:18:31 +0100

    Aggiunge KnpSnappyBundle ai bundle Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
