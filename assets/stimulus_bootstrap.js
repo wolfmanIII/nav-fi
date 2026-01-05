748c4ea0fffeb603dc571e5f70180488e6498d61
Space Wolf <wolfmanIII@gmail.com>
2026-01-05 20:24:57 +0100

    Abilita autoload Stimulus

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,16 +1,4 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-import AiConsoleController from './controllers/ai_console_controller.js';
-import BulkSelectController from './controllers/bulk_select_controller.js';
-import IncomeDetailsController from './controllers/income_details_controller.js';
-import ShipDetailsController from './controllers/ship_details_controller.js';
-import SingleSelectTableController from './controllers/single_select_table_controller.js';
-import YearLimitController from './controllers/year_limit_controller.js';
 
-const app = startStimulusApp();
-
-app.register('ai-console', AiConsoleController);
-app.register('bulk-select', BulkSelectController);
-app.register('income-details', IncomeDetailsController);
-app.register('ship-details', ShipDetailsController);
-app.register('single-select-table', SingleSelectTableController);
-app.register('year-limit', YearLimitController);
+// Controller autoload is configured via assets/controllers.json (StimulusBridge).
+startStimulusApp();
