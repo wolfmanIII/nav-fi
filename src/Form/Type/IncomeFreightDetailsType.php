b43585cf821648cf6d1a7a0692b555543cf171da
Space Wolf <wolfmanIII@gmail.com>
2026-01-06 01:53:06 +0100

    Datepicker imperiale nei dettagli Freight

--- src/Form/Type/IncomeFreightDetailsType.php
+++ src/Form/Type/IncomeFreightDetailsType.php
@@ -4,12 +4,15 @@ namespace App\Form\Type;
 
 use App\Entity\IncomeFreightDetails;
 use App\Form\Config\DayYearLimits;
+use App\Form\Type\ImperialDateType;
+use App\Model\ImperialDate;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class IncomeFreightDetailsType extends AbstractType
@@ -21,6 +24,11 @@ class IncomeFreightDetailsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $campaignStartYear = $options['campaign_start_year'] ?? null;
+        $minYear = $campaignStartYear ?? $this->limits->getYearMin();
+        /** @var IncomeFreightDetails|null $data */
+        $data = $builder->getData();
+        $pickupDate = new ImperialDate($data?->getPickupYear(), $data?->getPickupDay());
+        $deliveryDate = new ImperialDate($data?->getDeliveryYear(), $data?->getDeliveryDay());
         $builder
             ->add('origin', TextType::class, [
                 'required' => false,
@@ -32,25 +40,21 @@ class IncomeFreightDetailsType extends AbstractType
                 'label' => 'Destination',
                 'attr' => ['class' => 'input m-1 w-full'],
             ])
-            ->add('pickupDay', IntegerType::class, [
+            ->add('pickupDate', ImperialDateType::class, [
+                'mapped' => false,
                 'required' => false,
-                'label' => 'Pickup Day',
-                'attr' => $this->limits->dayAttr(['class' => 'input m-1 w-full']),
+                'label' => 'Pickup date',
+                'data' => $pickupDate,
+                'min_year' => $minYear,
+                'max_year' => $this->limits->getYearMax(),
             ])
-            ->add('pickupYear', IntegerType::class, [
+            ->add('deliveryDate', ImperialDateType::class, [
+                'mapped' => false,
                 'required' => false,
-                'label' => 'Pickup Year',
-                'attr' => $this->limits->yearAttr(['class' => 'input m-1 w-full'], $campaignStartYear),
-            ])
-            ->add('deliveryDay', IntegerType::class, [
-                'required' => false,
-                'label' => 'Delivery Day',
-                'attr' => $this->limits->dayAttr(['class' => 'input m-1 w-full']),
-            ])
-            ->add('deliveryYear', IntegerType::class, [
-                'required' => false,
-                'label' => 'Delivery Year',
-                'attr' => $this->limits->yearAttr(['class' => 'input m-1 w-full'], $campaignStartYear),
+                'label' => 'Delivery date',
+                'data' => $deliveryDate,
+                'min_year' => $minYear,
+                'max_year' => $this->limits->getYearMax(),
             ])
             ->add('cargoDescription', TextareaType::class, [
                 'required' => false,
@@ -84,6 +88,26 @@ class IncomeFreightDetailsType extends AbstractType
                 'label' => 'Cancellation terms',
                 'attr' => ['class' => 'textarea m-1 w-full', 'rows' => 2],
             ]);
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event): void {
+            /** @var IncomeFreightDetails $details */
+            $details = $event->getData();
+            $form = $event->getForm();
+
+            /** @var ImperialDate|null $pickup */
+            $pickup = $form->get('pickupDate')->getData();
+            if ($pickup instanceof ImperialDate) {
+                $details->setPickupDay($pickup->getDay());
+                $details->setPickupYear($pickup->getYear());
+            }
+
+            /** @var ImperialDate|null $delivery */
+            $delivery = $form->get('deliveryDate')->getData();
+            if ($delivery instanceof ImperialDate) {
+                $details->setDeliveryDay($delivery->getDay());
+                $details->setDeliveryYear($delivery->getYear());
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
