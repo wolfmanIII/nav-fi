b990d39b077261bb59deb15adc711eaa9618cd21
Space Wolf <wolfmanIII@gmail.com>
2026-01-07 01:16:40 +0100

    Allinea OperationGuideController

--- src/Controller/OperationGuideController.php
+++ src/Controller/OperationGuideController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class OperationGuideController extends AbstractController
+final class OperationGuideController extends BaseController
 {
+    const CONTROLLER_NAME = "OperationGuideController";
+
     #[Route('/operations/guide', name: 'app_operation_guide')]
     public function index(): Response
     {
-        return $this->render('guide/operations.html.twig');
+        return $this->render('guide/operations.html.twig', [
+            'controller_name' => self::CONTROLLER_NAME,
+        ]);
     }
 }
